7576f388fb9b4b916abdf8b530f993ed21ae2d1c
Kotchakorn Sanitkoon <89298632+hiiamnnn@users.noreply.github.com>
2022-05-08 01:41:24 +0700

    Add files via upload

---
+++ phpCodeToRetrieveDataFromHeroku/swiftui-get-rb-from-api.php
@@ -0,0 +1,20 @@
+<?php
+    //r&b genre
+    $conn = mysqli_connect("us-cdbr-east-04.cleardb.com", "bc16ebc3cf265a", "acc60a3c", "heroku_26e803d6028caf6");
+    // get data from database
+    $result = $conn->query("SELECT * FROM rbsongslist");
+ 
+    // adding in array
+    $data = array();
+    while ($row = mysqli_fetch_object($result))
+    {
+        //printf("%s %s %s %s %s \n", $row->idSongsList, $row->Title, $row->Artists, $row->Time, $row->Link);
+        array_push($data, $row);
+    }
+       
+    // send as response
+    echo json_encode($data);
+
+    
+    
+?>
